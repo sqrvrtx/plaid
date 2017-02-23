fa00bd50fce61f50c6864ead75b541bd68e1b608
Sqrvrtx <sqrvrtx@gmail.com>
2017-02-23 20:50:59 +0000

    removing click print

--- plaid/cli.py
+++ plaid/cli.py
@@ -6,7 +6,6 @@ import click
 @click.command()
 def main(args=None):
     """Console script for plaid"""
-    click.echo("See click documentation at http://click.pocoo.org/")
     import plaid
     plaid.main()
 
